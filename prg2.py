48ba79415ca0ebf7093597c25ff8623ac45113c0
jyothikasrikakulapu <148326117+jyothikasrikakulapu@users.noreply.github.com>
2025-07-02 21:13:30 +0530

    Create prg2.py

---
+++ prg2.py
@@ -0,0 +1,11 @@
+//TWO SUM
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_map={}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement],i]
+            num_map[num] = i
+        
